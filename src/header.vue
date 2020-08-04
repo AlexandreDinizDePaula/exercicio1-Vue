e9e9c863ff166338debe9a7b2e6fc2fa0f069da0
Alexandre Diniz de Paula <alexandre.dp@outlook.com>
2020-08-04 20:50:30 -0300

    init

---
+++ src/header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Logo</h1>
+
+        <a>Topic 1</a>
+        <a>Topic 2</a>
+        <a>Topic 3</a>
+        <a>Topic 4</a>
+
+        <input>
+    </div>
+</template>
